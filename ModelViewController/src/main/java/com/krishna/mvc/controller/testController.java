969652abaf91d8cb49566458321a0f4e7e8d132d
dshirsath <dshirsath@INPHWN157127.ad.harman.com>
2022-02-01 17:28:07 +0530

    updated controller

--- ModelViewController/src/main/java/com/krishna/mvc/controller/testController.java
+++ ModelViewController/src/main/java/com/krishna/mvc/controller/testController.java
@@ -2,11 +2,18 @@ package com.krishna.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class testController {
-	@RequestMapping("/test")
-	public String sayHello() {
+	
+	@RequestMapping("/testpage")
+	public String sayHelloPage() {
+		return "hello-world";
+	}
+	@ResponseBody
+	@RequestMapping("/testapi")
+	public String sayHelloAPI() {
 		return "hello-world";
 	}
 }
